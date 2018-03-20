03ed0b87f9a3a35f909d80afde89bf323aa4dcd9
Badri Narayan Mohan <badrinarayanmohan@gmail.com>
2018-03-20 19:20:59 +0530

    Initial Commit

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public void LoadLevel(string name)
+    {
+        Debug.Log("Level load requested for : " + name);
+        Application.LoadLevel(name);
+
+    }
+    public void QuitRequest()
+    {
+        Debug.Log("I WANNA QUIT!");
+        Application.Quit();
+    }
+
+    public void ReturnLevel(string name)
+    {
+        Debug.Log("Level load requested for : " + name);
+        Application.LoadLevel(name);
+
+    }
+}
---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+    // Use this for initialization
+    int max;
+    int min;
+    int guess;
+    int maxGuessesAllowed;
+    public Text text;
+
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        max = 1000;
+        min = 1;
+        guess = 500; 
+
+       maxGuessesAllowed = 10;
+
+
+        max = max + 1;
+    }
+    
+    public void GuessHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+    public void GuessLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+
+
+    void NextGuess()
+    {
+        guess = Random.Range(min,max+1);
+        text.text = guess.ToString();
+        maxGuessesAllowed = maxGuessesAllowed - 1;
+        if (maxGuessesAllowed <= 0)
+            Application.LoadLevel("Win");
+    }
+}
